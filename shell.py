734f511743c27096d9b603e7e337a71baa91dd77
Pabitra Banerjee <rockstarpabitra2204@gmail.com>
2023-10-28 08:34:16 +0530

    Creating .pc extension for PyConda file !!!

--- shell.py
+++ shell.py
@@ -2,25 +2,28 @@ import PyConda
 
 while True:
     text = input('PyConda > ')
-    
-    if text.strip() == "": 
-        continue
-    
-    if text.lower() in ["exit", "quit"]:
-        break  # Exit the loop and gracefully quit the program
-    
-    try:
-	if ".pc" not in text:
-		print("The provided file isn't a valid PyConda file.")
-        result, error = PyConda.run('<stdin>', text)
 
-        if error:
-            print(error.as_string())
-        elif result:
-            if len(result.elements) == 1:
-                print(repr(result.elements[0]))
-            else:
-                print(repr(result))
+    if text.strip() == "":
+        continue
+
+    if text.lower() in ["EXIT", "QUIT"]:
+        break
+
+    try:
+        if ".pc" not in text:
+            result = eval(text)
+            print(result)
+
+        else:
+            result, error = PyConda.run('<stdin>', text)
+
+            if error:
+                print(error.as_string())
+            elif result:
+                if len(result.elements) == 1:
+                    print(repr(result.elements[0]))
+                else:
+                    print(repr(result))
     except FileNotFoundError:
         print("File not found. Please enter a valid command.")
     except ValueError:
